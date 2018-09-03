e73a04dc427ba52eb264ace488b8a4be12778ba9
Abhi <ranzo9430@gmail.com>
2018-09-03 20:09:18 +0530

    update document

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -40,15 +40,61 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
         // console.log('Unable to fetch document', err);
         // });
 
-        db.collection('Users').find({name:"Abhishek"})
-        .toArray().then((docs)=>{
-        console.log(JSON.stringify(docs, undefined, 2));
+    //     db.collection('Users').find({name:"Abhishek"})
+    //     .toArray().then((docs)=>{
+    //     console.log(JSON.stringify(docs, undefined, 2));
         
-    },(err)=>{
-    console.log('Unable to fetch document', err);
-    });
+    // },(err)=>{
+    // console.log('Unable to fetch document', err);
+    // });
 
+    // Delete documents from Todos collections
+    
+    // db.collection('Todos').deleteMany({text: `Eat lunch`}).then((result)=>{
+    //     console.log(result);
+    // });
 
+    // db.collection('Todos').deleteOne({text:'Eat lunch'}).then((result)=>{
+    //        console.log(result);
+    // });
+     
+    // db.collection('Todos').findOneAndDelete({text:'Something to do'}).then((result)=>{
+    //     console.log(result);
+    // });
 
+    //db.collection('Users').deleteMany({name: 'fs'});
+    // db.collection('Users').findOneAndDelete({
+    //     _id: new ObjectID('5b88ba5c2c4d672b5decac5f')
+    //     }).then((result)=>{
+    //         console.log(JSON.stringify(result, undefined,2));
+    // })
+    //  db.collection('Todos').findOneAndUpdate({
+    //      _id:new ObjectID('5b865f228e0fff43bd8e763f')
+    //  },{
+    //    $set:{
+    //      completed: true
+    //    }  
+    //  },{
+    //     returnOriginal: false 
+    //  }).then((result)=>{
+    //      console.log(result);
+    //  })
+    db.collection('Users').findOneAndUpdate({
+        _id:new ObjectID('5b8654b22f446e1cd1490242')
+    },{
+      $set:{
+        name: 'Ambuj'
+      },
+      $inc:{
+          age:-2
+      } 
+    },
+    {
+       returnOriginal: false 
+    }).then((result)=>{
+        console.log(result);
+    })
+
+    
     // client.close();
 })
