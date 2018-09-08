4b44bf6ea22a373cce989a2b7bba1f4ef354eeca
Abhi <ranzo9430@gmail.com>
2018-09-08 09:49:33 +0530

    Add test for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,9 @@ const todos = [{
     text: 'First test todo'
 },{
     _id: new ObjectId(),
-    text:'Second test todo'
+    text:'Second test todo',
+    completed: true,
+    completedAt:333
 }];
 //Adding testing life cycle
 beforeEach((done)=>{
@@ -132,7 +134,55 @@ describe('DELETE /todos/:id',()=>{
      
   });
 
+});
+
+//how i like you to accomplish this
+//being rsourceful
+describe('should update the todo', (done)=>{
+    it('should update the todo', (done)=>{
+      //grab id of first item
+      var hexId = todos[0].id.toHexString();
+
+      //update text, set completed true
+      var text = 'This should be new text';
+      //200
+      request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+          completed:true,
+          text
+      })
+      .expect(200)
+      .expect((res)=>{
+         expect(res.body.todo.text).toBe(text);
+         expect(res.body.todo.completed).toBe(true);
+         expect(res.body.todo.completedAt).toBeA('number'); 
+      })
+      .end(done)
+      //text is chnaged, completed is tru, completeAt is a number, toBeA
+    });
+
+    it('should clear completedAt when todo is not completed', (done)=>{
+        //grab id of second todo item
+        var hexId = todos[1].id.toHexString();
+        //update text, set completed to false
+        var text = 'This should be new text!!!!!';
+        //200
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            completed:false,
+            text
+        })
+        .expect(200)
+        .expect((res)=>{
+           expect(res.body.todo.text).toBe(text);
+           expect(res.body.todo.completed).toBe(false);
+           expect(res.body.todo.completedAt).toNotExist(); 
+        })
+        .end(done)
+        //text  is changed, completed false, completed is null, toNotExist
 
 
-
+    });
 });
